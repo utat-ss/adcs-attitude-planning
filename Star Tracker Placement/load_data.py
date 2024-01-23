ad0b77f97223f3e6323b795e30bf4e2c574714cf
con266667 <cnwilson29@gmail.com>
2024-01-22 21:37:00 -0500

    template file reader

--- Star Tracker Placement/load_data.py
+++ Star Tracker Placement/load_data.py
@@ -4,4 +4,25 @@ def process_data(file_path):
 
     E.g. [[[time, sun x, sun y, sun z, ...], [time, sun x, sun y, sun z, ...]], ...]
     """
-    return []
+
+    with open(file_path) as f:
+        lines = f.readlines()
+
+    target_data = []
+    coords = []
+    block_count = 0
+
+    for line in lines:
+        if line[0] == '-':
+            block_count += 1
+            continue
+
+        if block_count == 1 and line != '\n':
+            target_data.append(line)
+        elif block_count == 2:
+            coords.append(line)
+
+    print(coords)
+    return target_data, coords
+
+process_data('FINCH_StarTracker_Sample.txt')
