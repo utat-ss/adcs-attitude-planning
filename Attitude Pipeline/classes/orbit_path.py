ec3d3dd8c1384590606f2aae5043564c000e0b6b
Stuart-Will <stuart.williamson@mail.utoronto.ca>
2024-03-23 17:24:58 -0400

    Implement Find Longest Path

--- Attitude Pipeline/classes/orbit_path.py
+++ Attitude Pipeline/classes/orbit_path.py
@@ -25,3 +25,48 @@ class OrbitPath:
 
         orbit = OrbitPath(passes)
         return orbit
+
+    def fragment_passes(self) -> None:
+        """ Fragment each imaging pass in the orbit into only those with all valid instances"""
+        new_passes = []
+        for img_pass in self.img_passes:
+            new_passes.extend(img_pass.fragment_to_valid())
+
+        self.img_passes = new_passes
+
+    def get_num_partValid_passes(self) -> int:
+        """ Return the number of imaging passes in the path that has at least one valid instance"""
+        num = 0
+        for img_pass in self.img_passes:
+            if len(img_pass.find_valid_indicies()) > 0:
+                num += 1
+        return num
+    
+    def get_num_fullValid_passes(self) -> int:
+        """ Return the number of imaging passes in the path that are fully valid"""
+        num = 0
+        for img_pass in self.img_passes:
+            if len(img_pass.instances) == len(img_pass.find_valid_indicies()):
+                num += 1
+        return num
+    
+    def get_longest_imaging_pass(self) -> tuple[ImagingPass,tuple[int,int]]:
+        """ Return the imaging pass that has the longest stretch of valid indicies and the start/end indicies of that stretch within the returned ImagingPass
+        """
+        longest_pass_len = 0
+        for img_pass in self.img_passes:
+            # Get the longest continuous stretch in the imaging pass
+            fragments, fragment_endpoints = img_pass.fragment_to_valid()
+            longest_fragment = fragments[0]
+            for fragment, endpoints in zip(fragments, fragment_endpoints):
+                if len(fragment.instances) > len(longest_fragment.instances):
+                    longest_fragment = fragment
+                    longest_fragment_endpoints = endpoints
+
+            # Compare with previous biggest
+            if len(longest_fragment.instances) > longest_pass_len:
+                longest_pass = img_pass
+                longest_endpoints = longest_fragment_endpoints
+                longest_pass_len = len(longest_pass.instances)
+
+        return longest_pass, longest_endpoints
