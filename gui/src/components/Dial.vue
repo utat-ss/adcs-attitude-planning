4b4a20e4d2aff636d9eaeaf9bc95a421445e0f7f
con266667 <cnwilson29@gmail.com>
2024-03-26 19:15:53 -0400

    dial template

---
+++ gui/src/components/Dial.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+defineProps<{
+  min: number,
+  max: number,
+  value: number,
+  title: string,
+}>()
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<style scoped>
+
+</style>
